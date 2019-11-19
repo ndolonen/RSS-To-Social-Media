ee20d56af4cd914468dd85051a11de732f6df678
Njål Dolonen <njal.dolonen@hoved.vadso.kommune.no>
2019-11-19 15:26:10 +0100

    changed implementation

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
+let fs = require('fs')
+let lastItem
+fs.readFile('lastitem.json', 'utf8', function(err, contents) {
+    lastItem = JSON.parse(contents)
+});
 
-let lastDay = new Date(getFullYear() + "-" + getMonth() + "-" + getDate())
+// let lastDay = new Date(getFullYear() + "-" + getMonth() + "-" + getDate())
 
 setInterval(async () => {
     let feed = await parser.parseURL('https://www.vadso.kommune.no/ArtikkelRSS.ashx?NyhetsKategoriId=9&Spraak=Norsk');
-    let today = new Date(getFullYear() + "-" + getMonth() + "-" + getDate())
-    let feedArray = []
+    if(!lastItem) {
+        lastItem = feed.items[0]
+        // console.log(JSON.stringify(lastItem))
+        fs.writeFileSync("lastitem.json", JSON.stringify(lastItem))
+    }
+    if(lastItem.guid != feed.items[0].guid) {
+        console.log("True")
+    }
+    else {
+        console.log("False")
+    }
 
-    feed.items.forEach(item => {
-        let today = new Date(getFullYear() + "-" + getMonth() + "-" + getDate())
-        if(lastDay != today) {
-            feedArray.clear()
-            lastday = today
-        }
+    // let today = new Date(getFullYear() + "-" + getMonth() + "-" + getDate())
+    // let feedArray = []
 
-        let now = new Date();
-        let d = new Date(item.isoDate)
+    // feed.items.forEach(item => {
+
+    //     let today = new Date(getFullYear() + "-" + getMonth() + "-" + getDate())
+    //     if(lastDay != today) {
+    //         feedArray.clear()
+    //         lastday = today
+    //     }
+
+    //     let now = new Date();
+    //     let d = new Date(item.isoDate)
         
-        let d1 = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate()
-        let d2 = now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate()
+    //     let d1 = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate()
+    //     let d2 = now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate()
         
-        if((d1 === d2) && (d.getMinutes >= now.getMinutes-5) && (true))
-        {
-            console.log(item)
-        }  
-    })
+    //     if((d1 === d2) && (d.getMinutes >= now.getMinutes-5) && (true))
+    //     {
+    //         console.log(item)
+    //     }  
+    // })
 
 }, 1800);
