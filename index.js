9e3ab6deea794a6977f1b975ceaa9a122a77bc35
Njål Dolonen <njal.dolonen@hoved.vadso.kommune.no>
2019-11-22 15:29:57 +0100

    First working edition

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let request = require('request');
 let Parser = require('rss-parser');
 let parser = new Parser();
 let fs = require('fs')
+let config = require("./config.json")
 let lastItem
 let postsFound = []
 fs.readFile('lastitem.json', 'utf8', function(err, content) {
@@ -16,26 +18,28 @@ fs.readFile('postsFound.json', 'utf8', function(err, content) {
     }
 });
 
-// let lastDay = new Date(getFullYear() + "-" + getMonth() + "-" + getDate())
-
 setInterval(async () => {
-    //test with reddit
-    let feed = await parser.parseURL('https://www.reddit.com/.rss');
-    if(!lastItem || lastItem.id != feed.items[0].id) {
+    // target feed for program
+    let feed = await parser.parseURL('https://www.vadso.kommune.no/ArtikkelRSS.ashx?NyhetsKategoriId=9&Spraak=Norsk');
+    if(!lastItem || lastItem.guid != feed.items[0].guid) {
         console.log("OPPDATERING: " + Date.now())
         lastItem = feed.items[0]
         fs.writeFileSync("lastitem.json", JSON.stringify(lastItem))
         postsFound.push(feed.items[0])
         fs.writeFileSync("postsFound.json", '{"posts": ' + JSON.stringify(postsFound) + '}')
+        await makeURI(feed.items[0].title, feed.items[0].link)
     }
-    
-    //target feed for program
-    // let feed = await parser.parseURL('https://www.vadso.kommune.no/ArtikkelRSS.ashx?NyhetsKategoriId=9&Spraak=Norsk');
-    // if(!lastItem || lastItem.guid != feed.items[0].guid) {
-    //     console.log("OPPDATERING: " + Date.now())
-    //     lastItem = feed.items[0]
-    //     fs.writeFileSync("lastitem.json", JSON.stringify(lastItem))
-    //     postsFound.push(feed.items[0])
-    //     fs.writeFileSync("postsFound.json", '{"posts": ' + JSON.stringify(postsFound) + '}')
-    // }
 }, 60000);
+
+
+async function makeURI(title, link) {
+    title = encodeURIComponent(title)
+    link = encodeURIComponent(link)
+    let URI = `https://graph.facebook.com/v5.0/me/feed?message=${title}&link=${link}&access_token=${config.access_token}`
+    request({
+        url: URI,
+        method: 'POST'
+    }, function(error, res, req){
+        console.log(req)
+    })
+}
