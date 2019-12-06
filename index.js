854f84132600ea8a3568f1d66d3236b290040e1b
Njål Dolonen <kongnjaal@gmail.com>
2019-12-06 10:57:41 +0100

    minor fix

--- index.js
+++ index.js
@@ -6,16 +6,16 @@
 */
 require('dotenv').config()
 let sqlite3 = require('sqlite3').verbose()
-let fs      = require('fs')
+let fs = require('fs')
 let request = require('request')
 var Twit = require('twit')
-let Parser  = require('rss-parser')
-let parser  = new Parser()
+let Parser = require('rss-parser')
+let parser = new Parser()
 const { BitlyClient } = require('bitly')
 const bitly = new BitlyClient(process.env.BITLY_ACCESS_TOKEN, {})
 
-let startDate = new Date()
-console.log("Started: " + startDate.getUTCHours() + ":" + startDate.getUTCMinutes() + ":" + startDate.getUTCSeconds())
+// let startDate = new Date()
+// console.log("Started: " + startDate.getUTCHours() + ":" + startDate.getUTCMinutes() + ":" + startDate.getUTCSeconds())
 
 //setup of data to be stored
 let lastItem = {
@@ -27,12 +27,12 @@ let lastItem = {
 getLast()
 
 let T = new Twit({
-  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-  access_token:         process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms:           60*1000,
-  strictSSL:            true,
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60*1000,
+  strictSSL: true,
 })
 
 //runs on a 1 minute interval and pings the RSS for updates
@@ -42,10 +42,10 @@ setInterval(async () => {
   let post_id = feed.items[0].id || feed.items[0].guid
   //posts if there is no prior posts or if the last posts id is not the same as the new one.
   if(!lastItem.post_id || lastItem.post_id != post_id) {
-    console.log("New post: " + new Date().getTime())
+    //console.log("New post: " + new Date().getTime())
     postToSocialMedia(feed.items[0].link, feed.items[0].title, feed.items[0].guid)
   }
-}, 3000)
+}, 60000)
 
 async function postToSocialMedia(link, title, post_id) {
 
@@ -70,7 +70,7 @@ async function postToSocialMedia(link, title, post_id) {
   let bitlyLink
   let linkStr = title + " " + link
   //Get a bitly link for twitter
-  if( linkStr.length >= 280 )
+  if( linkStr.length >= 279 )
   {
     try {
       bitlyLink = await bitly.shorten(link)
@@ -119,10 +119,10 @@ function Select(db) {
       }
       // console.log(typeof row.id)
       if(row){
-        lastItem.id         = "" + row.id
-        lastItem.post_id    = "" + row.post_id
+        lastItem.id = "" + row.id
+        lastItem.post_id = "" + row.post_id
         lastItem.post_title = "" + row.post_title
-        lastItem.fb_id      = "" + row.fb_id
+        lastItem.fb_id = "" + row.fb_id
       }
     }
   )
@@ -164,21 +164,21 @@ function writeError(source, error) {
   
   //sets the current date as the fileName  
   let fileName = source +
-    date.getUTCFullYear().toString()  + "_" + 
-    date.getUTCMonth().toString()     + "_" + 
-    date.getUTCDate().toString()      + "-" + 
-    date.getUTCHours().toString()     + "_" + 
-    date.getUTCMinutes().toString()   + "_" + 
-    date.getUTCSeconds().toString()   + ".log"
+    date.getUTCFullYear().toString() + "_" + 
+    date.getUTCMonth().toString() + "_" + 
+    date.getUTCDate().toString() + "-" + 
+    date.getUTCHours().toString() + "_" + 
+    date.getUTCMinutes().toString() + "_" + 
+    date.getUTCSeconds().toString() + ".log"
   
     //formats the error log
   let errorMessage = "" +
-    date.getUTCFullYear().toString()  + "/" + 
-    date.getUTCMonth().toString()     + "/" + 
-    date.getUTCDate().toString()      + " " + 
-    date.getUTCHours().toString()     + ":" + 
-    date.getUTCMinutes().toString()   + ":" + 
-    date.getUTCSeconds().toString()   + "\n\n" +
+    date.getUTCFullYear().toString() + "/" + 
+    date.getUTCMonth().toString() + "/" + 
+    date.getUTCDate().toString() + " " + 
+    date.getUTCHours().toString() + ":" + 
+    date.getUTCMinutes().toString() + ":" + 
+    date.getUTCSeconds().toString() + "\n\n" +
     error
   
   fs.writeFile(`./Error/${fileName}`, errorMessage, function (err) {
