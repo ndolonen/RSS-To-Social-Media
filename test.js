4f163e1a5199360789ccbd3868803e01bf6ad886
Njaal Dolonen <kongnjaal@gmail.com>
2019-12-10 14:56:09 +0100

    Delete test.js

--- test.js
+++
@@ -1,3 +0,0 @@
-let tekst = "Valg av brukerrepresentanter til eldrerådet og rådet for personer for med funksjonsnedsettelse "
-tekst += "https://www.vadso.kommune.no/aktuelt/valg-av-brukerrepresentanter-til-eldreradet-og-radet-for-personer-for-med-funksjonsnedsettelse.7145.aspx"
-console.log(tekst.length)
